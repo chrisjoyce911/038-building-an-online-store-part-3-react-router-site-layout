d08537c793c5c859e97c1dc76ef12d5846538582
Mark Tellez <mark.tellez@icloud.com>
2019-11-22 09:41:37 -0600

    added item count in top bar

--- src/components/top-bar/index.jsx
+++ src/components/top-bar/index.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./styles.css";
 import { Link } from "react-router-dom";
+import { CartContext } from "../cart/context";
 
 export default function TopBar() {
+  const cartCtx = useContext(CartContext);
+  const numItems = cartCtx.items.length;
   return (
     <div className="layout top-bar">
       <div className="wrapper">
         <div className="section">My store</div>
         <div className="section">
-          <Link to="/">Products</Link> | <Link to="/view-cart">View Cart</Link>
+          <Link to="/">Products</Link> |{" "}
+          <Link to="/view-cart">View Cart ({numItems})</Link>
         </div>
       </div>
     </div>
