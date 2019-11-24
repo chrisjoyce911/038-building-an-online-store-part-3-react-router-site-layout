7061e9edf401eabc38935e28d0593135d4655928
Mark Tellez <mark.tellez@icloud.com>
2019-11-24 17:31:51 -0600

    fixed item count in top bar

--- src/components/cart/context.js
+++ src/components/cart/context.js
@@ -1,38 +1,39 @@
-import React, { useState, createContext } from "react"
+import React, { useState, createContext } from "react";
 
-export const CartContext = createContext(null)
+export const CartContext = createContext(null);
 
 export default function CartProvider({ children }) {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
 
   function addToCart(item) {
-    setItems(prevState => [...prevState, item])
+    setItems(prevState => [...prevState, item]);
   }
 
   function itemsWithQuantities(items) {
     return items.reduce((acc, item) => {
-      const found = acc.find(_item => _item.sku === item.sku)
+      const found = acc.find(_item => _item.sku === item.sku);
       if (found) {
-        found.quantity = found.quantity + 1
+        found.quantity = found.quantity + 1;
       } else {
         acc.push({
           sku: item.sku,
           quantity: 1,
           price: item.price
-        })
+        });
       }
-      return acc
-    }, [])
+      return acc;
+    }, []);
   }
 
   return (
     <CartContext.Provider
       value={{
         items: itemsWithQuantities(items),
+        itemsCount: items.length,
         addToCart
       }}
     >
       {children}
     </CartContext.Provider>
-  )
+  );
 }
